4618f8bf44d9325e4c71774743c475b8e86e1d30
Omelianov Nicolai <omelianov.n@gmail.com>
2023-03-12 13:27:22 +0200

    added 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+const body = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+function backgroundChangeColor() {
+    const newColor = getRandomHexColor()
+    body.style.backgroundColor = newColor
+}
+
+let timer
+stopBtn.disabled = true
+
+startBtn.addEventListener('click', () => {
+    timer = setInterval(backgroundChangeColor, 1000)
+    startBtn.disabled = true
+    stopBtn.disabled = false
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timer)
+    startBtn.disabled = false
+    stopBtn.disabled = true
+})
+
+
+
+
+
